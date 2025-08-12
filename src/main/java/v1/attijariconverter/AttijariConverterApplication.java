7a720ed9e1e619c9963ebaa71d2eabde3375d2fd
ayman <Ayman.HilalYounes@emsi-edu.ma>
2025-08-12 09:26:11 +0100

    first commit

---
+++ src/main/java/v1/attijariconverter/AttijariConverterApplication.java
@@ -0,0 +1,13 @@
+package v1.attijariconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttijariConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttijariConverterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/v1/attijariconverter/AttijariConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package v1.attijariconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttijariConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
